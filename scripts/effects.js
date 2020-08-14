dc8fd027e3c862339ad66d6a91c06f440dd64266
Asher Lipman <41AsherL@gmail.com>
2020-08-14 18:50:40 -0400

    Credit alert implementation

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    //credit alert
+    alert("I completed this project as part of a team of 4 students. My responsibilities included the 'Calendar' page, the 'Jiu-Jitsu Basics page', the navigation and footer bars, and overall site responsiveness. Thank you.")
 
     //navigation responsiveness//
     var nav_open=0;
